62bbc5d3d3a4e0364a0d8e6793263fdb73b7ed3b
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-02-09 12:29:58 -0500

    guests crud functionality

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-/**
- * /events
- * {
-    id: 1,
-    name: "Event Name",
-    description: "This is a description of the event.",
-    date: "2021-09-30T00:00:00.000Z", // Date ISO string
-    location: "123 Street"   
-   }
- * 
- */
 // API URL
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2411-FSA-ET-WEB-PT/events`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2411-FSA-ET-WEB-PT/`;
 
 const state = {
   events: [],
+  guests: [],
 };
+
+async function init() {
+  await fetchAllEvents();
+  await fetchAllGuests();
+  addListenerToForm();
+}
+
+init();
+
+// ==============================
+// EVENTS API
+// ==============================
 
 async function fetchAllEvents() {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(`${API_URL}/events`);
     const json = await response.json();
     state.events = json.data;
     renderAllEvents();
   } catch (e) {
-    console.log(e);
-  } finally {
-    console.log(state);
+    console.error("Error fetching events: ", e);
+    return [];
   }
 }
 
 async function deleteEvent(id) {
   try {
-    console.log(id);
-    await fetch(`${API_URL}/${id}`, {
+    await fetch(`${API_URL}/events/${id}`, {
       method: "DELETE",
     });
     fetchAllEvents();
   } catch (e) {
-    console.log(e);
+    console.error(`Error deleting event #${id}: `, e);
   }
 }
 
 async function addEvent(name, description, date, location) {
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${API_URL}/events`, {
       method: "POST",
       body: JSON.stringify({
         name,
@@ -57,7 +57,7 @@ async function addEvent(name, description, date, location) {
     const json = await response.json();
     fetchAllEvents();
   } catch (e) {
-    console.log(e);
+    console.error("Error adding event: ", e);
   }
 }
 
@@ -138,9 +138,51 @@ function renderAllEvents() {
   });
 }
 
-async function init() {
-  await fetchAllEvents();
-  addListenerToForm();
+// ==============================
+// GUESTS API
+// ==============================
+
+async function fetchAllGuests() {
+  try {
+    const response = await fetch(`${API_URL}/guests`);
+    const json = await response.json();
+    state.guests = json.data;
+  } catch (e) {
+    console.error("Error fetching guests: ", e);
+    return [];
+  }
 }
 
-init();
+async function deleteGuest(id) {
+  try {
+    await fetch(`${API_URL}/guests/${id}`, {
+      method: "DELETE",
+    });
+    fetchAllGuests;
+  } catch (e) {
+    console.error(`Error deleting guest #${id}: `, e);
+  }
+}
+
+async function addGuest(name, email, phone, cohortId) {
+  try {
+    const newGuest = {
+      name,
+      email,
+      phone,
+      cohortId,
+    };
+
+    // post request
+    const response = await fetch(`${API_URL}/guests`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newGuest),
+    });
+
+    if (!response.ok) throw new Error("Failed to add guest");
+    await fetchAllGuests();
+  } catch (e) {
+    console.error("Error adding guest: ", e);
+  }
+}
